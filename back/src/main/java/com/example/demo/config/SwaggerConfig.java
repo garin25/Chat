45d91f5848d0addba098abcec2b761e76a056412
garin25 <garinchristian4@gmail.com>
2026-01-28 13:45:28 -0300

    doc con swagger

---
+++ back/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Chat en Tiempo Real 🚀")
+                        .version("1.0")
+                        .description("Documentación de los endpoints del sistema de chat"))
+                // Configuración para el botón de JWT
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
